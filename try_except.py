374700ddf4e246b9bdce2e17829c93190aaeb48b
Ghaith <ghaithkhedhri3@gmail.com>
2024-06-15 17:19:17 +0100

    GGG

--- try_except.py
+++ try_except.py
@@ -1,13 +1,20 @@
+def main():
+    x=get_int("what's x? ")
+    print(f"this x is an integer. {x}")
+def get_int(prompt):
+    while True:
+        #try something
+        try:
+            x=int(input(prompt))
+            #we can just delete else and just type in here return x, but i needed to show you the else in the try except
+            #return x
 
-while True:
-    #try something
-    try:
-        x=int(input("what's x? "))
+        #except something goes wrong
+        except ValueError:
+            pass
+        #if nothing goes wrong, else:
+        else:
+            #instead of typing break, we will do return x, return is a stronger command line it returns the value and break from the loop.
+            return x
 
-    #except something goes wrong
-    except ValueError:
-        print("error, re-type your x.it should be an integer. i'm  asking you again, ")
-    #if nothing goes wrong, else:
-    else:
-        break
-print(f"x is {x}")
+main()
