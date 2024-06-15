17e8dbbc195de6be4f9405f42c959f33c0f85bb0
Ghaith <ghaithkhedhri3@gmail.com>
2024-06-15 16:51:01 +0100

    ,

--- try_except.py
+++ try_except.py
@@ -1,10 +1,12 @@
-#try something
-try:
-    x=int(input("what's x? "))
 
-#except something goes wrong
-except ValueError:
-    print('error, re-type your x. you just typed: it should be an integer. ') 
-#if nothing goes wrong, else:
-else:
-    print(f"this is {x}")
+while True:
+    #try something
+    try:
+        x=int(input("what's x? "))
+
+    #except something goes wrong
+    except ValueError:
+        print('error, re-type your x.it should be an integer. ')
+    #if nothing goes wrong, else:
+    else:
+        print(f"this is {x}")
