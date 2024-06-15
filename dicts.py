854266ed38a52a8fec9ad9c7371bbb6c3d6ac2a2
Ghaith <ghaithkhedhri3@gmail.com>
2024-06-15 14:36:48 +0100

    adding/editing parts for a full view about python.

---
+++ dicts.py
@@ -0,0 +1,10 @@
+students=[
+    {"name":"rainz","house":"west appartement","patronus":"outter"},
+    {"name":"sys","house":"east","patronus":"easter"},
+    {"name":"clair","house":"herbitic house","patronus":"ice"},
+    {"name":"odiciz","house":"helland","patronus":None}
+]
+for student in students:
+    print (student,students[student] sep=", ")
+    print(students[student])
+    print (student)
---
+++ listing.py
@@ -0,0 +1,5 @@
+students=["ghaith","mahmoud","tobo","hazem"]
+students=["Gryffindor","Gryffindor","Gryffindor","Slytherin"]
+print("print first students name in the list: ", students[0])
+for i in range(len(students)):
+    print(i+1, students[i])
