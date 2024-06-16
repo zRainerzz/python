7f793b36597823a465368118c3c286f34171e2e8
Ghaith <ghaithkhedhri3@gmail.com>
2024-06-16 23:09:48 +0100

    progressing in the volenteer small project bit

--- libraries3sys.py
+++ libraries3sys.py
@@ -1,13 +1,19 @@
 import sys
 
-
+#check for errors:
 if len(sys.argv)<3:
-    print("too few arguments")
+    #sys.exit with the system's help, it's gonna exit my program then and there on line 6
+    sys.exit("too few arguments")
 
 elif len(sys.argv)>3:
-    print("too many")
+    #sys.exit with the system's help, it's gonna exit my program then and there on line 10
+    sys.exit("too many arguments")
 
-else:
-    # comment
-    #in terminal just type python <filename>.py <name>
-    print("hello, my name is", sys.argv[2])
+#printing here:
+# comment
+#in terminal just type python <filename>.py <name>
+print("hello, my name is", sys.argv[2])
+
+
+#incase you want to show your full name, just type in the terminal: python libraries3sys.py "david smith"
+# that way, it will appear, fully. 
