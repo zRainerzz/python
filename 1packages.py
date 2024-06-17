d3e10de1f253cbd177d98bef2a7444e514ba2594
Ghaith <ghaithkhedhri3@gmail.com>
2024-06-17 01:41:23 +0100

    package1 is cowsay

---
+++ 1packages.py
@@ -0,0 +1,7 @@
+import cowsay
+import sys
+#packages are all useful and some are funny, like this one... named cowsay i just installed it in my terminal by triggering this command: pip install cowsay
+
+
+if len(sys.argv)==2:
+    cowsay.cow("hello, "+ sys.argv[1])
