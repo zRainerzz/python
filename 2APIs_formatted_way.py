0af95081e68b31c2bafa1b8c50eb53602b8719f5
Ghaith <ghaithkhedhri3@gmail.com>
2024-06-17 18:53:38 +0100

    better layout

--- 2APIs_formatted_way.py
+++ 2APIs_formatted_way.py
@@ -5,8 +5,15 @@ import sys
 if len(sys.argv) != 2 :
     print("system will exit, invalid input.")
     sys.exit()
-request=requests.get("https://itunes.apple.com/search?entity=song&limit=1&term="+ sys.argv[1])
-print(json.dumbs(request.json(), indent=2))
+
+
+request=requests.get("https://itunes.apple.com/search?entity=song&limit=50&term="+ sys.argv[1])
+#print(json.dumps(request.json(), indent=2))
+s=request.json()
+
+for result in s["results"]:
+    print(result["trackName"])
+
 #i'd like things to be nicely intended and according to the documentation, so i pass in a named paramter of indetn, which will provide me with double spaces.(you can space however you want depending on the value u give to the indent.)
 
 #json.dumbs() this one format the the json type files in a nice and more readable way.
