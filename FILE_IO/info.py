127df9531d4e10abfd0a40b183fdca79c90d198a
Ghaith <ghaithkhedhri3@gmail.com>
2024-06-27 08:52:56 +0100

    .

--- FILE_IO/info.py
+++ FILE_IO/info.py
@@ -3,7 +3,7 @@ import csv
 informations=[]
 x=input("Type input to write and print to show the list: ")
 if x=="input":
-    prename=input("given name of then new person is:  ").title()
+    prename=input("given name of the new person is:  ").title()
     name=input(f"{prename}'s last name is: ").title()
     town=input(f"{prename} {name} is from: ")
     city=input(f"{name} {prename} lives in {town}, city name is: ").title()
@@ -17,12 +17,12 @@ if x=="input":
 elif x=="print":
     with open ("info.csv") as file:
         #reader's purpose in life is to read csv files for me and figure out where are commas or quotes, where are the potential corner cases and just deal with them for me...
-        #we can use it instead, but i don't have to at the moment. here's the input as example reader=
+        #we can use it instead, but i don't have to at the moment. here's the input as example reader=csv.reader
         for line in file:
             prename,name,town,city,hood=line.rstrip().split(",")
             information={"prename":prename,"name":name,"town":town,"city":city,"hood":hood}
             informations.append(information)
-            print(f"{information['name']} {information['prename']} is from {town}, he lives in {information['hood']},{information['city']}.")
+            print(f"_{name} {prename}_ is from _{town}_, he lives in _{hood}_,_{city}_.")
 #if you haven't noticed yet, i just made a new .csv file which is used to store multiple pieces of information in the same file, it is very commonly used when you #use something like microsoft excel, apple numbers or google spreedsheets and you want to export the data to share with someone else as a csv file or conversly if you #want to import a csv file into your preferred spreedsheet software like excel or numbers or google spreedsheets you can do that as well so csv is a vey simple #text format that just seperates values with commas and different types of values ultimately with new lines as well.
 else:
     print("invalid input, you should type input to add or print to show.")
