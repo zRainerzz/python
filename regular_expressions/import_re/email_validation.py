9becf5789ab64757501473e4084a4cb39c1fdb9f
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-03 14:33:40 +0100

    .

--- regular_expressions/import_re/email_validation.py
+++ regular_expressions/import_re/email_validation.py
@@ -3,7 +3,7 @@ import re
 #re.search(pattern, string, flags=0)
 #for more info jump to note.txt
 email=input("Whats ur email? ").strip()
-if re.search(r"^\w+@[^@]+\.(net|com|org)$", email):
+if re.search(r"^\w+@[^@]+\.(net|com|org)$", email.lower()):
 #[^@] means take everything but another @ 
 #[abcdefghijklmnopqrstuvwxyz1234567890_] now i'm using everything between the brackets numbers and alphabets or just type [a-zA-Z0-9_] or \w to represent that you want a "word characters."
 #^ means start matching at the beginning of the string,   \w means word characters only,   + means one or more,   @ at symbol literally,   [^@] everything but @,   + means 1 or more,   \.com a dot com literally,   $ match at the end   
