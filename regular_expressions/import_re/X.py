e3dfc75210e9d25f2bf04f8407ff85517707be2e
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-04 09:15:23 +0100

    .

--- regular_expressions/import_re/X.py
+++ regular_expressions/import_re/X.py
@@ -1,10 +1,11 @@
 import re
 url=input("URL: ").strip()
-
-username=re.sub(r"^(https?://)?(www\.|)twitter\.com","",url    )
+matches=re.search(r"^https?://(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
+if matches:
+    print(f"Username: ", matches.group(1))
+#username=re.sub(r"^(https?://)?(www\.|)twitter\.com","",url    )
 #username=url.removeprefix("https://twitter.com/","")
 #prefix is a string that comes at the start of another, so if i remove prefix i wont need another argument for this function.
 
 #username=url.replace("https://twitter.com/","")
 #you pass 2 arguments in replace, what do you want to replace and what do you want to replace it with
-print(f"Username: {username}")
