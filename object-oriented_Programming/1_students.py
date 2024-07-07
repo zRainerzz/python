e1e90e3db1b606d3b8b85364e54b3a3b4a554687
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-07 12:15:58 +0100

    .

--- object-oriented_Programming/1_students.py
+++ object-oriented_Programming/1_students.py
@@ -1,14 +1,14 @@
 def main():
     student=get_student()
-    if student[0]=="Padma":
-        student[1]="RavenClaw"
-    print(f"{student[0]} from {student[1]}").capitalize()
+    if student["name"]=="padma" or student["name"]=="Padma":
+        student["house"]="ravenclaw"
+    print(f"{student['name']} from {student['house']}").capitalize()
 
 
 def get_student():
     name=input("Name: ")
     house=input("House: ")
-    return [name, house]
+    return {"name":name, "house":house}
 
 
 if __name__ == "__main__":
---
+++ object-oriented_Programming/classes.py
@@ -0,0 +1,18 @@
+#class is like a blueprint for pieces of data (objects) 
+#a class is like a mold that you can define and give a name when you use that mold or use that blueprint you get types of data that are designed as you want.
+#in short, classes allow you to invent your own data types in Python and give them a name and this is a primary feature of object oriented programming to be able to create your own objects this way and in case of python in classes. even give them some custom names.
+class Student:
+    ...
+def main():
+    student=get_student()
+    print(f"{student.name} from {student.house}")
+
+
+def get_student():
+    student=Student()
+    student.name=input("Name: ").capitalize()
+    student.house=input("House: ").capitalize()
+    return student
+
+if __name__ == "__main__":
+    main()
