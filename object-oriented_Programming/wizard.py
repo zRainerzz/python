3f696240d0c681f4c9708f4a8822a180d4412aca
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-15 23:03:40 +0100

    will finish the job tomorrow, barely alive thanks to the surgery.

---
+++ object-oriented_Programming/wizard.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self,name,house):
+        if not name:
+            raise ValueError("Missing name.")
+        self.name=name
+        self.house=house
+
+
+
+
+class Professor:
+    def __init__(self,name,subject):
+        if not name:
+            raise ValueError("Missing name.")
+        self.name=name
+        self.subject=subject
