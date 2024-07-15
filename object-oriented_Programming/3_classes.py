d7fce4f5e11d5de2fe8a06b5ad83a32fc1c14687
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-15 10:05:43 +0100

    surgery commit 4

--- object-oriented_Programming/3_classes.py
+++ object-oriented_Programming/3_classes.py
@@ -2,7 +2,7 @@
 #a class is like a mold that you can define and give a name when you use that mold or use that blueprint you get types of data that are designed as you want.
 #in short, classes allow you to invent your own data types in Python and give them a name and this is a primary feature of object oriented programming to be able to create your own objects this way and in case of python in classes. even give them some custom names.
 class Student:
-    def __init__ (self,first,middle, last , house, patronus):
+    def __init__ (self,first,middle, last , house,):
         if not first:
             raise ValueError("Invalid name, No first name? ")
         if not last:
@@ -14,27 +14,15 @@ class Student:
         self.middle=middle
         self.last=last
         self.house=house
-        self.patronus=patronus
         #def __str__(self):
         #use it to print the message, instead of printing it from the main function. it will have the priority
-    def charm(self):
         #__init__ and __str__ are special methods (functions in classes are called methods) and now we are creating our own method.
-        match self.patronus:
-            case "Stag":
-                return "🐴"
-            case "Otter":
-                return "🦦"
-            case "Jack Russle terrier":
-                return "🐶"
-            case _:
-                return "No patronus = No charm :)"
 def main():
     student=get_student()
-    print("expecto patronum!")
     if not student.middle:
-        print(f"{student.first} {student.last} from {student.house} and patronus is: {student.patronus} charm: ", student.charm())
+        print(f"{student.first} {student.last} from {student.house}")
     else:
-        print(f"{student.first} {student.middle} {student.last} from {student.house} and patronus is: {student.patronus} charm:", student.charm())
+        print(f"{student.first} {student.middle} {student.last} from {student.house}")
 
 
 def get_student():
@@ -42,8 +30,7 @@ def get_student():
     middle=input("Middle Name (optional): ").capitalize()
     last=input("Last Name: ").capitalize()
     house=input("House: ").capitalize()
-    patronus=input("Patronus: ").capitalize()
-    return Student(first, middle, last, house, patronus)
+    return Student(first, middle, last, house)
 
 if __name__ == "__main__":
     main()
