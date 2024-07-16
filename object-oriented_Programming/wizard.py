c5a6cfd717248719094c62a6559f90a407598cac
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-16 10:30:01 +0100

    .

--- object-oriented_Programming/wizard.py
+++ object-oriented_Programming/wizard.py
@@ -1,16 +1,20 @@
-class Student:
-    def __init__(self,name,house):
+class Wizard:
+    def __init__ (self,name):
         if not name:
-            raise ValueError("Missing name.")
+            raise ValueError("Missing name. ")
         self.name=name
+
+
+class Student(Wizard):
+#this is called inheritance,class Student(Wizard): now the class Student inherit all the characteristics of the Wizard class.
+    def __init__(self,house):
+        super().__init__(name)
+        #super is the reference to the super class of this class so if this class is student the super class, the parent class is wizard
         self.house=house
 
 
 
 
-class Professor:
-    def __init__(self,name,subject):
-        if not name:
-            raise ValueError("Missing name.")
-        self.name=name
+class Professor(Wizard):
+    def __init__(self,subject):
         self.subject=subject
