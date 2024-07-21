d78d4ea00b66b9bcabb839683e74d2ca87d2bf49
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-21 13:53:36 +0100

    .

--- p_type_hints.py
+++ p_type_hints.py
@@ -1,6 +1,12 @@
 def meow(n:int) -> None:
     """
     it shows that my meow function by design returns none so literally use this arrow notation on python when hinting what the return value
+    
+    :param n: Number of times to meow
+    :type n: int
+    :raise TypeError: if n is not an int
+    :return: A string of n meows, one per line
+    :rtype: str
     """
     return f"meow\n"*n
 number : int =int(input("Number: "))
