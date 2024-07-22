654348fb1a01ba30a310133e2fc81030fdd1ee45
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-22 15:26:16 +0100

    .

--- tool_2_generators.py
+++ tool_2_generators.py
@@ -15,5 +15,7 @@ if __name__ == "__main__":
 """
 imagine if you can print 1m sheeps without forcing the capability of the processor?
 yeah, instead of forcing the processor to print 1m sheeps at once, you can make a generator that will
-generate sheeps seperately, little by little until it reaches 1m, without the processor knowing that it is generating that
+generate sheeps seperately, little by little until it reaches 1m, without the processor knowing that it is generating that massive amount of data.
+instead of you printing 1 sheep and run the program 1m times, it will automatically run itself 1m times.
+THANK YOU PYTHON!
 """
