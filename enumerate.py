f1c172c7ff511fb4cbb3e7883d0d03b812268bd7
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-22 14:31:16 +0100

    .

---
+++ enumerate.py
@@ -0,0 +1,10 @@
+students=["Hermoine","Harry","Ron"]
+
+for i, student in enumerate(students):
+    print(i,+1, student)
+
+    """
+    so with enumerate you won't need an index into the list with bracket i notation,
+    you dont need to call range or len 
+    because enumerate takes a sequence of values like these students and allows me to get back the current index 0 1 2 and the current value Hermoine Harry Ron
+    """
