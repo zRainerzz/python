d641f1f4c18925d239a0738ddbe2be28d72fc247
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-22 14:16:17 +0100

    .

---
+++ dictcomprehension_1.py
@@ -0,0 +1,4 @@
+students=["Hermoine","Harry","Ron"]
+
+gryffindors = {student:"Gryffindor" for student in students}
+print(gryffindors)
---
+++ listcomprehension_4.py
@@ -0,0 +1,4 @@
+students=["Hermoine","Harry","Ron"]
+
+gryffindors = [{"name":student, "house":"Gryffindor"} for student in students]
+print(gryffindors)
